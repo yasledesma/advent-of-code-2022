7fd132b4f2c7b86b68a5e4485c7c747690179f53
yasledesma <yaslesd@gmail.com>
2023-07-20 23:35:08 -0300

    feat(day-3): add part 2

--- day-3/main.go
+++ day-3/main.go
@@ -9,17 +9,26 @@ import (
 )
 
 func main() {
-    var score int
+    var priorityOne int
+    var priorityTwo int
+    var elfs = make([]string, 0, 3)    
+    
     input := OpenFile("input.txt")
     
     for input.Scan() {
         line := input.Text()
-        middleIndex := len(line) / 2
         
-        score += CalculateItems(line[:middleIndex], line[middleIndex:])
+        priorityOne += CalcPartOne(line)
+
+        elfs = append(elfs, line)
+
+        if len(elfs) == 3 {
+            priorityTwo += CalcPartTwo(elfs[0], elfs[1], elfs[2])
+            elfs = elfs[:0]
+        }
     }
 
-    fmt.Print(score)
+    fmt.Print(priorityOne, priorityTwo)
 }
 
 func OpenFile(input string) *bufio.Scanner {
@@ -32,12 +41,37 @@ func OpenFile(input string) *bufio.Scanner {
     return bufio.NewScanner(file)
 }
 
-func CalculateItems(comp1, comp2 string) int {
+func CalcPartOne(line string) int {
+    middleIndex := len(line) / 2
+    return CalcItems(line[:middleIndex], line[middleIndex:])
+}
+
+func CalcPartTwo(lineOne, lineTwo, lineThree string) int {
     var points int
     letters := "abcdefghijklmnopqrstuvwxyz"
     
-    for _, v := range comp1 {
-        if strings.Contains(comp2, string(v)) {
+    for _, v := range lineOne {
+        if strings.Contains(lineTwo, string(v)) {
+            if strings.Contains(lineThree, string(v)){
+                if strings.Index(letters, string(v)) != -1 {
+                    points += strings.Index(letters, string(v)) + 1
+                } else {
+                    points += strings.Index(letters, strings.ToLower(string(v))) + 27 
+                }
+                break
+            }
+        }
+    }
+    
+    return points 
+}
+
+func CalcItems(string1, string2 string) int {
+    var points int
+    letters := "abcdefghijklmnopqrstuvwxyz"
+    
+    for _, v := range string1 {
+        if strings.Contains(string2, string(v)) {
             if strings.Index(letters, string(v)) != -1 {
                 points += strings.Index(letters, string(v)) + 1
             } else {
