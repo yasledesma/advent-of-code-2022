0d909e72aae32a25f5d66df3f09e27b744e9da59
yasledesma <yaslesd@gmail.com>
2023-07-25 23:45:18 -0300

    feat(day-5): add part 2

--- day-5/main.go
+++ day-5/main.go
@@ -1,3 +1,4 @@
+// This is the shitties, most umperformant solution ever. I should have used channels in some way.
 package main
 
 import (
@@ -12,7 +13,8 @@ import (
 
 func main() {
     var move [3]int
-    stack := map[int]string{}
+    stackOne := map[int]string{}
+    stackTwo := map[int]string{}
     
     file := utils.OpenFile("input.txt")
 
@@ -23,7 +25,8 @@ func main() {
         if strings.Contains(line, "[") {
             for j := 0; j < len(line); j++ {
                 if string(line[j]) != " " && string(line[j]) != "[" && string(line[j]) != "]" {
-                    stack[(i-1)/4] += string(line[j])
+                    stackOne[(i-1)/4] += string(line[j])
+                    stackTwo[(i-1)/4] += string(line[j])
                 }
                 i++
             }
@@ -32,17 +35,26 @@ func main() {
         if strings.Contains(line, "move") {
             fmt.Sscanf(line, "move %d from %d to %d", &move[0], &move[1], &move[2])
             
+            // PART 1 PROCESS
             for j := 0; j < move[0]; j++ {
-                rest := string(stack[move[1]][1:])
-                letter := string(stack[move[1]][0])
+                restOne := string(stackOne[move[1]][1:])
+                letter := string(stackOne[move[1]][0])
                 
-                stack[move[1]] = rest
-                stack[move[2]] = letter + stack[move[2]]
+                stackOne[move[1]] = restOne
+                stackOne[move[2]] = letter + stackOne[move[2]]
             }
+
+            // PART 2 PROCESS
+            restTwo := string(stackTwo[move[1]][move[0]:])
+            letters := string(stackTwo[move[1]][0:move[0]])
+
+            stackTwo[move[1]] = restTwo
+            stackTwo[move[2]] = letters + stackTwo[move[2]]
         }
     }
     
-    fmt.Println(stack)
+    fmt.Println(stackOne)
+    fmt.Println(stackTwo)
 }
 
 
