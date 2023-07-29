82b6f017313dbdb5b272a97f78576cc61a642965
yasledesma <yaslesd@gmail.com>
2023-07-28 21:48:16 -0300

    feat(day-6): add part 2

--- day-6/main.go
+++ day-6/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-    var mark = 4
-    var start = 0
+    var markOne = 4
+    var startOne = 0
     
     file, err := os.ReadFile("input.txt")
 
@@ -18,13 +18,38 @@ func main() {
 
     // PART 1 
     for {
-        current := file[start:mark]
-        if current[3] != current[2] && current[3] != current[1] && current[3] != current[0] && current[2] != current[1] && current[2] != current[0] && current[1] != current[0]  {
-            fmt.Println(mark)
+        current := file[startOne:markOne]
+        
+        if current[3] != current[2] && current[3] != current[1] && current[3] != current[0] && current[2] != current[1] && current[2] != current[0] && current[1] != current[0] {
             break
         }
 
-        mark++
-        start++
+        markOne++
+        startOne++
     }
+
+    // PART 2
+    var markTwo = 14 
+    var startTwo = 0
+    var stream = make(map[byte]string)
+    var currenTwo = file[startTwo:markTwo]
+    j := 0
+    
+    for {
+        if len(stream) == 14 {
+            break
+        } else if stream[currenTwo[j]] == "" {
+            stream[currenTwo[j]] = fmt.Sprint(j)
+            j++
+        } else {
+            stream = map[byte]string{}
+            startTwo++
+            markTwo++
+            currenTwo = file[startTwo:markTwo]
+            j = 0
+        }
+    }
+
+    
+    fmt.Println(markOne, markTwo)
 }
